66548ada0ea54a42ee592b760f31eefee083a1f9
WDev9911 <longdtse184444@fpt.edu.vn>
2025-06-17 00:16:23 +0700

    feat(product): implement DeleteProduct

---
+++ Services/Catalog/CatalogAPI/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductResponse(bool IsSuccess);
+
+
+
+public class DeleteProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products/{id}",
+            async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(id)); 
+
+                var respone = result.Adapt<DeleteProductResponse>();
+
+                return Results.Ok(respone); // Trả về HTTP 200 OK với thông tin kết quả xóa sản phẩm
+            })
+            .WithName("DeleteProduct")
+            .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product");
+
+    }
+}
