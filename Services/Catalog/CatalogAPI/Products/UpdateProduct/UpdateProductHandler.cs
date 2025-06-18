a0f4a5116db4f4df1febfc2d2fb2403a676d2155
WDev9911 <longdtse184444@fpt.edu.vn>
2025-06-18 19:01:14 +0700

    feat: add validation rules for UpdateProduct

--- Services/Catalog/CatalogAPI/Products/UpdateProduct/UpdateProductHandler.cs
+++ Services/Catalog/CatalogAPI/Products/UpdateProduct/UpdateProductHandler.cs
@@ -11,30 +11,42 @@ public record UpdateProductCommand (
 
 public record UpdateProductResult(bool ISuccess);
 
-
-
-internal class UpdateProductCommandHandler (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
 {
-    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    public UpdateProductCommandValidator()
     {
-        logger.LogInformation("UpdateProductCommandHandler.Handle called with {@Request}", command);
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is not in database.");
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Product name is required.").Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Product category is required.");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Product price must be greater than zero.");
+    }
 
-        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
 
-        if (product is null)
+
+    internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
         {
-            throw new ProductNotFoundException();
+            logger.LogInformation("UpdateProductCommandHandler.Handle called with {@Request}", command);
+
+            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+            if (product is null)
+            {
+                throw new ProductNotFoundException(command.Id);
+            }
+
+            product.Name = command.Name;
+            product.Category = command.Category;
+            product.Description = command.Description;
+            product.ImageFile = command.ImageFile;
+            product.Price = command.Price;
+
+            session.Update(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new UpdateProductResult(true);
+
         }
-
-        product.Name = command.Name;
-        product.Category = command.Category;
-        product.Description = command.Description;
-        product.ImageFile = command.ImageFile;
-
-        session.Update(product);
-        await session.SaveChangesAsync(cancellationToken);
-
-        return new UpdateProductResult(true);
-
     }
 }
