437627ee1d1d3462bfdab39006d617248a725d8d
WDev9911 <longdtse184444@fpt.edu.vn>
2025-07-07 17:59:30 +0700

    Defined `ShoppingCart.cs` and `ShoppingCartItem.cs` for basket entities.

---
+++ Services/Basket/Basket.API/Models/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingCartItem
+{
+    public int Quantity { get; set; } = default!;
+    public string Color { get; set; } = default!; // Assuming Color is a string, adjust as necessary
+    public decimal Price { get; set; } = default!;
+
+    public Guid ProductId { get; set; } = default!; // Assuming ProductId is a Guid, adjust as necessary
+
+    public string ProductName { get; set; } = default!; // Assuming ProductName is a string, adjust as necessary
+}
